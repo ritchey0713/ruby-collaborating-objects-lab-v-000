c2c142ae995a01fe88362fc5f68f541f5fbbb463
David Ritchey <ritchey0713@gmail.com>
2018-04-04 20:09:32 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,8 +14,7 @@ class MP3Importer
   end
   
     def import
-    # binding.pry
-    self.files.each do |file|
+    files.each do |file|
       song = Song.new_by_filename(file)
       Artist.all << song.artist unless Artist.all.include?(song.artist)
 end
